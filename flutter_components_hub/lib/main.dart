ff3d02498a88f15e6446f34f044d73ad2cb20cd5
Chinmoy Kumar Biswas <bangladeshchinmoy@gmail.com>
2025-08-20 20:27:23 +0600

    Updated folder

--- flutter_components_hub/lib/main.dart
+++ flutter_components_hub/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components_hub/Flutter_UI_Components/Main_Components/Buttons/social_icon_buttons.dart';
+import 'package:flutter_components_hub/Flutter_UI_Components/Main_Components/Skeleton_Loader/skeleton_loader_1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,39 +12,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Title')),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SocialIconButton(
-                platform: SocialPlatform.google,
-                onPressed: () {},
-              ),
-              SizedBox(width: 16.0),
-              SocialIconButton(
-                platform: SocialPlatform.facebook,
-                onPressed: () {},
-              ),
-              SizedBox(width: 16.0),
-
-              SocialIconButton(
-                platform: SocialPlatform.twitter,
-                onPressed: () {},
-              ),
-              SizedBox(width: 16.0),
-
-              SocialIconButton(
-                platform: SocialPlatform.instagram,
-                onPressed: () {},
-              ),
-              SizedBox(width: 16.0),
-
-              SocialIconButton(
-                platform: SocialPlatform.linkedin,
-                onPressed: () {},
-              ),
-            ],
+        appBar: AppBar(title: const Text('Skeleton Loader Example')),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: SkeletonLoader(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SkeletonPlaceholder(
+                        width: 50.0,
+                        height: 50.0,
+                        borderRadius: 25.0, // For a circular shape
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SkeletonPlaceholder(width: 150),
+                            const SizedBox(height: 8),
+                            SkeletonPlaceholder(),
+                            const SizedBox(height: 8),
+                            const SkeletonPlaceholder(width: 120),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
