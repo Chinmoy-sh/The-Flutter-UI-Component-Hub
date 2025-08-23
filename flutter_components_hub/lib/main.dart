d6eed695bae4dbd26f685286d95a0a688c698b67
Chinmoy Kumar Biswas <bangladeshchinmoy@gmail.com>
2025-08-23 17:47:10 +0600

    Update main.dart

--- flutter_components_hub/lib/main.dart
+++ flutter_components_hub/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components_hub/Flutter_UI_Components/Main_Components/Skeleton_Loader/universal_loader.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,49 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: UniversalSkeletonLoader(
-            child: Padding(
-              padding: EdgeInsets.all(24.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SkeletonPlaceholder(width: 200.0, height: 24.0),
-                  SizedBox(height: 16.0),
-                  SkeletonPlaceholder(width: double.infinity, height: 16.0),
-                  SizedBox(height: 8.0),
-                  SkeletonPlaceholder(width: double.infinity, height: 16.0),
-                  SizedBox(height: 8.0),
-                  SkeletonPlaceholder(width: 150.0, height: 16.0),
-                  SizedBox(height: 32.0),
-                  Row(
-                    children: <Widget>[
-                      SkeletonPlaceholder(
-                        width: 40.0,
-                        height: 40.0,
-                        borderRadius: 20.0,
-                      ),
-                      SizedBox(width: 16.0),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            SkeletonPlaceholder(width: 120.0, height: 14.0),
-                            SizedBox(height: 4.0),
-                            SkeletonPlaceholder(width: 80.0, height: 10.0),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
